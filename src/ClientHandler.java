11dea384b447e3634a91b3a36548e2347e139629
wonilkim-wastaken <is0646ep@ed.ritsumei.ac.jp>
2025-12-01 11:59:56 +0900

    Created ClientHandler skeleton

---
+++ src/ClientHandler.java
@@ -0,0 +1,30 @@
+import java.net.Socket;
+
+public class ClientHandler {
+    int user;
+    Socket socket;
+
+    private int createUser(){
+
+    }
+
+    private void sendConnectRequest(int userID) {
+
+    }
+
+    private void queryActiveUsers() {
+
+    }
+
+    private void sendMessage(String message) {
+
+    }
+
+    private void connectToServer(String hostName, int port) {
+
+    }
+
+    private void reveiveMessage(String message) {
+
+    }
+}
